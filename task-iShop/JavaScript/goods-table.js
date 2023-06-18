9c076d8949399fc139a109bcaa2d553ad20c4448
Andrei Shket <andrewshkt@gmail.com>
2023-06-18 13:28:42 +0300

    New fonts and store name added

--- task-iShop/JavaScript/goods-table.js
+++ task-iShop/JavaScript/goods-table.js
@@ -12,6 +12,7 @@ const GoodsTable = React.createClass({
         productNumber: React.PropTypes.number.isRequired,
       }),
     ),
+    shopName: React.PropTypes.string.isRequired,
   },
 
   render: function() {
@@ -19,18 +20,22 @@ const GoodsTable = React.createClass({
     const tableItems = this.props.data.map(a => React.createElement(TableItems, {key: a.productNumber, 
       name: a.phoneModel, image: a.phoneImage, price: a.phonePrice, stockQuantity: a.stockQuantity})
     );
-    
-    return React.DOM.table(null, 
-      React.DOM.thead(null, 
-        React.DOM.tr(null, 
-          React.DOM.th(null, "Phone Model"),
-          React.DOM.th(null, "Image"),
-          React.DOM.th(null, "Price"),
-          React.DOM.th(null, "Amount"),
-        ),
-      ), 
-      React.DOM.tbody(null, tableItems),
+
+    return React.DOM.div({className: "goods-table-container"}, 
+      React.DOM.h1(null, "iShop"),
+      React.DOM.table(null, 
+        React.DOM.thead(null, 
+          React.DOM.tr(null, 
+            React.DOM.th(null, "Phone Model"),
+            React.DOM.th(null, "Image"),
+            React.DOM.th(null, "Price"),
+            React.DOM.th(null, "Amount"),
+          ),
+        ), 
+        React.DOM.tbody(null, tableItems),
+      )
     );
+    
   },
 
 });
